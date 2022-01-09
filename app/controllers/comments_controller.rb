d278bd90d2e6e9c610f5ddc044e9b81e1f0e0a15
Guty Santana <agustin.santana@gmail.com>
2022-01-09 17:34:54 -0600

    Adding comments to Articles, and changing layout

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+
+    redirect_to article_path(@article) if @comment.save
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+end
